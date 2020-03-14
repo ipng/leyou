833a73a5bc82c60905dc4594a295039bf0b53147
ipng <572188879@qq.com>
2020-03-14 23:53:33 +0800

    1.0.2

--- leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/CategoryController.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/controller/CategoryController.java
@@ -1,9 +1,16 @@
 package com.leyou.item.controller;
 
+import com.leyou.item.pojo.Category;
 import com.leyou.item.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.CollectionUtils;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 /**
  * Created by Enzo Cotter on 2020/3/10.
@@ -13,4 +20,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
+
+    /**
+     * 由父节点的ID查询子节点
+     * @param pid
+     * @return
+     */
+    @GetMapping("list")
+    public ResponseEntity<List<Category>> queryNamesByIds(@RequestParam(value="pid",defaultValue = "0") Long pid) {
+
+            if(pid == null||pid < 0){
+//                400
+//                return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+                return ResponseEntity.badRequest().build();
+            }
+            List<Category> categories= this.categoryService.queryCategoriesByPid(pid);
+            if(CollectionUtils.isEmpty(categories)){
+//                404
+//                return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+                return ResponseEntity.notFound().build();
+            }
+//            200
+            return ResponseEntity.ok(categories);
+
+    }
 }
--- leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/CategoryService.java
+++ leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/CategoryService.java
@@ -1,8 +1,11 @@
 package com.leyou.item.service;
 
 import com.leyou.item.mapper.CategoryMapper;
+import com.leyou.item.pojo.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * Created by Enzo Cotter on 2020/3/10.
@@ -11,4 +14,15 @@ import org.springframework.stereotype.Service;
 public class CategoryService {
     @Autowired
     private CategoryMapper categoryMapper;
+
+    /**
+     * 父节点查询节点
+     * @param pid
+     * @return
+     */
+        public List<Category> queryCategoriesByPid(Long pid) {
+        Category record = new Category();
+        record.setParentId(pid);
+        return this.categoryMapper.select(record);
+    }
 }
